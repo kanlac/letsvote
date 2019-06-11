60fab91fa3c43721e99e954609a5d6c8c89fed8b
serfusE <serfuse@gmail.com>
2019-06-11 10:01:10 +0800

    add about page.

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,6 @@
 from flask import render_template, flash, url_for, session, redirect, request
 from . import main
+
+@main.route('/about', methods=['GET'])
+def about():
+	return render_template('about.html')
